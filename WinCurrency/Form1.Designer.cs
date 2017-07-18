74e5fb667fc407bc8efa4ab5f76c6922bb673276
davidchiang <davidjhun1235@gmail.com>
2017-07-18 15:13:24 +0800

    OK

--- WinCurrency/Form1.Designer.cs
+++ WinCurrency/Form1.Designer.cs
@@ -28,12 +28,130 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblMonthMinRate = new System.Windows.Forms.Label();
+            this.tbxMonthMinRate = new System.Windows.Forms.TextBox();
+            this.lblHYearMinRate = new System.Windows.Forms.Label();
+            this.tbxHYearMinRate = new System.Windows.Forms.TextBox();
+            this.listCurrency = new System.Windows.Forms.ListBox();
+            this.tbxMonthMinDate = new System.Windows.Forms.TextBox();
+            this.tbxHYearMinDate = new System.Windows.Forms.TextBox();
+            this.lblToday = new System.Windows.Forms.Label();
+            this.tbxTodayRate = new System.Windows.Forms.TextBox();
+            this.tbxTodayDate = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblMonthMinRate
+            // 
+            this.lblMonthMinRate.AutoSize = true;
+            this.lblMonthMinRate.Location = new System.Drawing.Point(47, 161);
+            this.lblMonthMinRate.Name = "lblMonthMinRate";
+            this.lblMonthMinRate.Size = new System.Drawing.Size(33, 12);
+            this.lblMonthMinRate.TabIndex = 1;
+            this.lblMonthMinRate.Text = "label1";
+            // 
+            // tbxMonthMinRate
+            // 
+            this.tbxMonthMinRate.Location = new System.Drawing.Point(124, 151);
+            this.tbxMonthMinRate.Name = "tbxMonthMinRate";
+            this.tbxMonthMinRate.Size = new System.Drawing.Size(100, 22);
+            this.tbxMonthMinRate.TabIndex = 2;
+            // 
+            // lblHYearMinRate
+            // 
+            this.lblHYearMinRate.AutoSize = true;
+            this.lblHYearMinRate.Location = new System.Drawing.Point(49, 204);
+            this.lblHYearMinRate.Name = "lblHYearMinRate";
+            this.lblHYearMinRate.Size = new System.Drawing.Size(33, 12);
+            this.lblHYearMinRate.TabIndex = 3;
+            this.lblHYearMinRate.Text = "label2";
+            // 
+            // tbxHYearMinRate
+            // 
+            this.tbxHYearMinRate.Location = new System.Drawing.Point(124, 204);
+            this.tbxHYearMinRate.Name = "tbxHYearMinRate";
+            this.tbxHYearMinRate.Size = new System.Drawing.Size(100, 22);
+            this.tbxHYearMinRate.TabIndex = 4;
+            // 
+            // listCurrency
+            // 
+            this.listCurrency.FormattingEnabled = true;
+            this.listCurrency.ItemHeight = 12;
+            this.listCurrency.Location = new System.Drawing.Point(49, 12);
+            this.listCurrency.Name = "listCurrency";
+            this.listCurrency.Size = new System.Drawing.Size(120, 64);
+            this.listCurrency.TabIndex = 5;
+            this.listCurrency.SelectedIndexChanged += new System.EventHandler(this.listCurrency_SelectedIndexChanged);
+            // 
+            // tbxMonthMinDate
+            // 
+            this.tbxMonthMinDate.Location = new System.Drawing.Point(230, 151);
+            this.tbxMonthMinDate.Name = "tbxMonthMinDate";
+            this.tbxMonthMinDate.Size = new System.Drawing.Size(100, 22);
+            this.tbxMonthMinDate.TabIndex = 6;
+            // 
+            // tbxHYearMinDate
+            // 
+            this.tbxHYearMinDate.Location = new System.Drawing.Point(230, 201);
+            this.tbxHYearMinDate.Name = "tbxHYearMinDate";
+            this.tbxHYearMinDate.Size = new System.Drawing.Size(100, 22);
+            this.tbxHYearMinDate.TabIndex = 7;
+            // 
+            // lblToday
+            // 
+            this.lblToday.AutoSize = true;
+            this.lblToday.Location = new System.Drawing.Point(49, 116);
+            this.lblToday.Name = "lblToday";
+            this.lblToday.Size = new System.Drawing.Size(33, 12);
+            this.lblToday.TabIndex = 8;
+            this.lblToday.Text = "label1";
+            // 
+            // tbxTodayRate
+            // 
+            this.tbxTodayRate.Location = new System.Drawing.Point(124, 105);
+            this.tbxTodayRate.Name = "tbxTodayRate";
+            this.tbxTodayRate.Size = new System.Drawing.Size(100, 22);
+            this.tbxTodayRate.TabIndex = 9;
+            // 
+            // tbxTodayDate
+            // 
+            this.tbxTodayDate.Location = new System.Drawing.Point(230, 105);
+            this.tbxTodayDate.Name = "tbxTodayDate";
+            this.tbxTodayDate.Size = new System.Drawing.Size(100, 22);
+            this.tbxTodayDate.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(588, 373);
+            this.Controls.Add(this.tbxTodayDate);
+            this.Controls.Add(this.tbxTodayRate);
+            this.Controls.Add(this.lblToday);
+            this.Controls.Add(this.tbxHYearMinDate);
+            this.Controls.Add(this.tbxMonthMinDate);
+            this.Controls.Add(this.listCurrency);
+            this.Controls.Add(this.tbxHYearMinRate);
+            this.Controls.Add(this.lblHYearMinRate);
+            this.Controls.Add(this.tbxMonthMinRate);
+            this.Controls.Add(this.lblMonthMinRate);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private System.Windows.Forms.Label lblMonthMinRate;
+        private System.Windows.Forms.TextBox tbxMonthMinRate;
+        private System.Windows.Forms.Label lblHYearMinRate;
+        private System.Windows.Forms.TextBox tbxHYearMinRate;
+        private System.Windows.Forms.ListBox listCurrency;
+        private System.Windows.Forms.TextBox tbxMonthMinDate;
+        private System.Windows.Forms.TextBox tbxHYearMinDate;
+        private System.Windows.Forms.Label lblToday;
+        private System.Windows.Forms.TextBox tbxTodayRate;
+        private System.Windows.Forms.TextBox tbxTodayDate;
     }
 }
 
--- WinCurrency/Form1.cs
+++ WinCurrency/Form1.cs
@@ -7,6 +7,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using HtmlAgilityPack;
+using System.IO;
+using System.Net;
+
+
 
 namespace WinCurrency
 {
@@ -15,6 +20,139 @@ namespace WinCurrency
         public Form1()
         {
             InitializeComponent();
+            ddlCurrency();
+            lblHYearMinRate.Text = "半年最小值";
+            lblMonthMinRate.Text = "當月最小值";
+            lblToday.Text = "本日匯率";
         }
+
+        public void ddlCurrency()
+        {
+            string[] currency = new string [3];
+            currency[0] = "USD";
+            currency[1] = "JPY";
+            currency[2] = "EUR";
+            listCurrency.DataSource = currency;
+        }
+       
+
+        
+        private void listCurrency_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Rate thisMonth = new Rate("thisMonth", listCurrency.Text);
+            thisMonth.getRate();
+            tbxMonthMinRate.Text = thisMonth.RateMin.ToString();
+            tbxMonthMinDate.Text = thisMonth.DateMin;
+            tbxTodayRate.Text = thisMonth.TodayRate.ToString();
+            tbxTodayDate.Text = thisMonth.TodayDate;
+            
+
+            Rate HalfYear = new Rate("HalfYear", listCurrency.Text);
+            HalfYear.getRate();
+            tbxHYearMinRate.Text = HalfYear.RateMin.ToString();
+            tbxHYearMinDate.Text = HalfYear.DateMin;
+
+            
+        }
+    }
+
+    public class Rate
+    {
+        string webUrl;
+        string rType;
+        string Currency;
+        public double RateMin;
+        public string DateMin;
+
+        MemoryStream ms;
+        WebClient url = new WebClient();
+        string date = DateTime.Now.Year.ToString() + "-" + ("0" + DateTime.Now.Month.ToString()).Substring(0, 2);
+
+        string h1;
+        string h2;
+        //string cDate;
+
+        string[] rDate;
+        double[] rRate;
+
+        public string TodayDate;
+        public double TodayRate;
+
+        HtmlAgilityPack.HtmlDocument doc;
+        HtmlAgilityPack.HtmlDocument hdc;
+        HtmlNodeCollection htnode;
+
+        double rate;
+
+
+
+
+
+        public Rate(string type, string currency)
+        {
+            rType = type;
+            Currency = currency;
+        }
+
+
+
+        public void getRate()
+        {
+
+            if (rType == "thisMonth")
+            {
+                webUrl = @"http://rate.bot.com.tw/xrt/quote/" + date + "/" + Currency;
+
+            }
+            else if (rType == "HalfYear")
+            {
+                webUrl = @"http://rate.bot.com.tw/xrt/quote/l6m/" + Currency;
+            }
+
+            ms = new MemoryStream(url.DownloadData(webUrl));
+            doc = new HtmlAgilityPack.HtmlDocument();
+            doc.Load(ms, Encoding.UTF8);
+
+            hdc = new HtmlAgilityPack.HtmlDocument();
+            hdc.LoadHtml(doc.DocumentNode.SelectSingleNode(" / html[1]/body[1]/div[1]/main[1]/div[4]/table[1]/tbody[1]").InnerHtml);
+            htnode = hdc.DocumentNode.SelectNodes("./tr");
+
+            rDate = new string[htnode.Count];
+            rRate = new double[htnode.Count];
+
+            for (int a = 0; a < htnode.Count; a++)
+            {
+                if (Currency != "KRW")
+                {
+                    h1 = string.Format("./tr[{0}]/td[1]", a + 1);
+                    h2 = string.Format("./tr[{0}]/td[4]", a + 1);
+                }
+                else //韓元沒即期匯率，所以抓現金匯率
+                {
+                    h1 = string.Format("./tr[{0}]/td[1]", a + 1);
+                    h2 = string.Format("./tr[{0}]/td[6]", a + 1);
+                }
+                rDate[a] = (hdc.DocumentNode.SelectSingleNode(h1).InnerText);
+                rRate[a] = Convert.ToDouble(hdc.DocumentNode.SelectSingleNode(h2).InnerText);
+            }
+
+            rate = rRate.Min();
+
+            RateMin = rate;
+            int index = Array.IndexOf(rRate, rate);
+
+            TodayDate = rDate[0];
+            TodayRate = rRate[0];
+
+            DateMin = rDate[index];
+
+
+
+
+        }
+
+
+
+
     }
 }
